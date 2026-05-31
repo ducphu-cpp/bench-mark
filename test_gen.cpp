ef74e0c9088d810ee9ccb17fbb7eae77cf4c2b55
Phu Huynh Duc <huynhducphu10082007@gmail.com>
2026-05-31 11:58:14 +0700

    Update test_gen.cpp

--- test_gen.cpp
+++ test_gen.cpp
@@ -1,117 +1,84 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <random>
+#include <chrono>
+
 using namespace std;
 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-int RandInt(int l, int r) {
-    return uniform_int_distribution<int>(l, r)(rng);
+int32_t generate_random_int32(mt19937_64& rng) {
+    uniform_int_distribution<long long> dist(-2147483648LL, 2147483647LL);
+    return static_cast<int32_t>(dist(rng));
 }
 
-char RandChar() {
-    return 'a' + RandInt(0, 25);
-}
+void generate_int_test(int test_num) {
+    int n = 100000;
+    cout << n << "\n";
+    mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-
-void genInt(int testid) {
-
-    int n;
-
-    if (testid == 1) n = 1000;
-    else if (testid == 2) n = 5000;
-    else if (testid == 3) n = 20000;
-    else if (testid == 4) n = 50000;
-    else n = 100000;
-
-    cout << n << '\n';
-
-    for (int i = 0; i < n; i++) {
-        cout << RandInt(INT_MIN, INT_MAX) << '\n';
-    }
-}
-
-
-void genStrLexi(int testid) {
-
-    int n;
-
-    if (testid == 1) n = 1000;
-    else if (testid == 2) n = 5000;
-    else if (testid == 3) n = 20000;
-    else if (testid == 4) n = 50000;
-    else n = 100000;
-
-    cout << n << '\n';
-
-    for (int i = 0; i < n; i++) {
-
-        int len = RandInt(80, 120);
-
-        char c = RandChar();
-
-        string s(len, c);
-
-        s[len - 1] = RandChar();
-
-        cout << s << '\n';
-    }
-}
-
-void genStrLenLexi(int testid) {
-
-    int n;
-
-    if (testid == 1) n = 1000;
-    else if (testid == 2) n = 5000;
-    else if (testid == 3) n = 20000;
-    else if (testid == 4) n = 50000;
-    else n = 100000;
-
-    cout << n << '\n';
-
-    for (int i = 0; i < n; i++) {
-
-        int len = RandInt(90, 100);
-
-        string s(len, 'a');
-
-        int changes = RandInt(1, 3);
-
-        for (int j = 0; j < changes; j++) {
-
-            int pos = RandInt(len - 5, len - 1);
-
-            s[pos] = RandChar();
+    if (test_num == 3) {
+        uniform_int_distribution<long long> dist_positive(-500000000LL, 2147483647LL);
+        for (int i = 0; i < n; ++i) {
+            cout << static_cast<int32_t>(dist_positive(rng)) << "\n";
         }
-
-        cout << s << '\n';
+    } else {
+        for (int i = 0; i < n; ++i) {
+            cout << generate_random_int32(rng) << "\n";
+        }
     }
 }
 
+void generate_strlexi_test(int test_num) {
+    int n = 10000;
+    cout << n << "\n";
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+    for (int i = 0; i < n; ++i) {
+        string s(98, 'a');
+        s += (char)('a' + rng() % 26);
+        s += (char)('a' + rng() % 26);
+        cout << s << "\n";
+    }
+}
+
+void generate_strlenlexi_test(int test_num) {
+    int n = 100000;
+    cout << n << "\n";
+    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+    for (int i = 0; i < n; ++i) {
+        char repeated_char = 'a' + (rng() % 26);
+        string s(100, repeated_char);
+        cout << s << "\n";
+    }
+}
 
 int main(int argc, char* argv[]) {
-
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    if (argc != 3) {
-        return 0;
+    if (argc < 3) {
+        cerr << "Sai cú pháp! Định dạng chuẩn: " << argv[0] << " <int/strlexi/strlenlexi> <1/2/3/4/5>\n";
+        return 1;
     }
 
-    string type = argv[1];
-    int testid = stoi(argv[2]);
+    string benchmark_type = argv[1];
+    int test_num = stoi(argv[2]);
 
-    if (testid < 1 || testid > 5) {
-        return 0;
+    if (test_num < 1 || test_num > 5) {
+        cerr << "Số thứ tự bộ test phải nằm trong khoảng từ 1 đến 5.\n";
+        return 1;
     }
 
-    if (type == "int") {
-        genInt(testid);
-    }
-    else if (type == "strlexi") {
-        genStrLexi(testid);
-    }
-    else if (type == "strlenlexi") {
-        genStrLenLexi(testid);
+    if (benchmark_type == "int") {
+        generate_int_test(test_num);
+    } 
+    else if (benchmark_type == "strlexi") {
+        generate_strlexi_test(test_num);
+    } 
+    else if (benchmark_type == "strlenlexi") {
+        generate_strlenlexi_test(test_num);
+    } 
+    else {
+        cerr << "Loại dữ liệu không hợp lệ! Vui lòng chọn 'int', 'strlexi', hoặc 'strlenlexi'.\n";
+        return 1;
     }
 
     return 0;
